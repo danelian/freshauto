1478c92b97a9c6a6108623feae3fea3e020b555f
danelian <141317269+danelian@users.noreply.github.com>
2024-01-31 02:04:32 +0300

    header - adaptation

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,28 @@
-$(document).ready(function () {
+document.addEventListener('DOMContentLoaded', function(){
 
-});
+  // ------------------- BURGER MENU ---------------------
+  const body = document.querySelector('body'),
+        burgerMenu = document.querySelector('#burger-menu'),
+        burgerOpen = document.querySelector('#burger-open'),
+        burgerClose = document.querySelector('#burger-close'),
+        burgerLinks = document.querySelectorAll('.header__menu>li>a, .header__bar a');
+  burgerOpen.addEventListener('click', () => {
+    burgerMenu.classList.add('show');
+    body.classList.add('dis-scroll');
+  })
+  burgerClose.addEventListener('click', () => {
+    burgerMenu.classList.remove('show');
+    body.classList.remove('dis-scroll')
+  })
+  burgerLinks.forEach(n => n.addEventListener('click', () => {
+    burgerMenu.classList.remove('show');
+    body.classList.remove('dis-scroll')
+  }))
+  document.addEventListener('click', (event) => {
+    if (!event.target.matches("#burger-menu, #burger-open, #burger-open img")) {
+      burgerMenu.classList.remove('show');
+      body.classList.remove('dis-scroll')
+    }
+  })
+
+})
